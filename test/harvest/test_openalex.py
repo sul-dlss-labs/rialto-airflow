bb285248115e2df00a9049757a2f732f3c54dc09
Laura Wrubel <lwrubel@stanford.edu>
2024-07-08 11:35:23 -0400

    Restore assertion

--- test/harvest/test_openalex.py
+++ test/harvest/test_openalex.py
@@ -50,6 +50,7 @@ def test_publications_from_dois():
     pubs = list(openalex.publications_from_dois(dois))
     assert len(pubs) == 231, "should paginate (page size=200)"
     assert len(pubs) == len(set([pub["doi"] for pub in pubs])), "DOIs are unique"
+    assert set(openalex.FIELDS) == set(pubs[0].keys()), "All fields accounted for."
     assert len(pubs[0].keys()) == 51, "first publication has 51 columns"
     assert len(pubs[1].keys()) == 51, "second publication has 51 columns"
 
